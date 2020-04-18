abe48464f03855bb81011bd98dc65ff44ee95caa
bsihame <sihamebazi@pursuit.org>
2020-04-17 23:57:01 -0400

    add code.js

---
+++ src/components/Clock.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log(new Date().toLocaleDateString());
+      setTime(new Date().toLocaleTimeString())
+    },1000)
+    return() => clearInterval(timer)
+  }, []);
+  return (
+    <div>
+      {time}
+    </div>
+  )
+}
+
+export default Clock
